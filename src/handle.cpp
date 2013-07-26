4c4886c9eb089c373bfab7d100c1837a94bc05fa
Will <willusher.life@gmail.com>
2013-07-26 16:32:43 -0700

    Some improvements to Handle, if thedefault c'tor is used no memory is allocated and instead we detect the nullptr and return -1 if GLuint() is called

--- src/handle.cpp
+++ src/handle.cpp
@@ -2,18 +2,16 @@
 #include <SDL_opengl.h>
 #include "../include/handle.h"
 
-GL::Handle::Handle() : mObj(nullptr) {
-	mObj = std::shared_ptr<GLuint>(new GLuint(-1));
-}
+GL::Handle::Handle() : mObj(nullptr) {}
 GL::Handle::Handle(GLuint obj, std::function<void(GLuint*)> del) 
-	: mObj(nullptr)
-{
-	mObj = std::shared_ptr<GLuint>(new GLuint(obj), del);
-}
+	: mObj(new GLuint(obj), del)
+{}
 void GL::Handle::release(){
 	if (mObj != nullptr)
 		mObj.reset();
 }
 GL::Handle::operator GLuint() const {
+	if (mObj == nullptr)
+		return -1;
     return *mObj.get();
 }
